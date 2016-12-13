e67f75cc5d5965566867ff4febe0b4983fcccc19
Jelani <jelanithompson@hotmail.com>
2016-12-13 14:10:46 -0500

    Configuration files

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './src/scripts/main.js',
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["babel-loader"],
+            }
+        ],
+    },
+
+    output: {
+        path: './src/scripts/output/',
+        filename: 'bundle.js'
+    }
+}
