c961c27f6e71415313ab6b264dbdd6a967bfbf35
Jelani Thompson <jelani.thompson@outlook.com>
2016-12-18 15:57:50 -0500

    Added playbar component

---
+++ src/components/Player/Player.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Player extends React.Component {
+    play(url) {}
+
+    pause() {}
+
+    render() {
+        return (
+            { /* Playbar and components */ }
+        )
+    }
+}
+
+export default Player
