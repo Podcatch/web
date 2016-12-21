9f0e8080add4f55b8452b292b19893586500e89f
Jelani Thompson <jelani.thompson@outlook.com>
2016-12-20 22:07:36 -0500

    Material UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import Login from '../Button/Login.js'
-import Signup from '../Button/Signup.js'
+import AppBar from 'material-ui/AppBar';
 
 class Header extends React.Component {  
     render() {
         return (
-                <div id="header">
-                    <i className="material-icons" id="menu-icon">menu</i>
-                    <i className="material-icons">person</i>
-                    <Login/>
-                    <Signup/>
-                    <div id="title">Podcatch</div>
-                </div>
+            <AppBar 
+                title="Podcatch"
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+            />
         )
     }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 // Import components
 import Header from '../components/Header/Header.js'
 import Sidebar from '../components/Sidebar/Sidebar.js'
 import Footer from '../components/Footer/Footer.js'
 
+injectTapEventPlugin()
+
 class App extends React.Component {
     render() {
         return(
-            <div>
+            <MuiThemeProvider>
                 <Header/>
-                <Sidebar/>
-                <Footer/>
-            </div>
+            </MuiThemeProvider>
         )
     }
 }
