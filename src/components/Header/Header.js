3dd870718b0bb89fa3ba0a2c33b961f334329e5f
Jelani Thompson <jelani.thompson@outlook.com>
2016-12-23 20:30:59 -0500

    Working on state with burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
-class Header extends React.Component {  
+class Header extends React.Component {
+    // Sidebar is not shown by default
+    constructor(props) {
+        super(props)
+        this.state = {
+            sidebarVisible: false
+        }
+    }
+
+    showSidebar() {
+        this.setState({
+            sidebarVisible: !this.state.sidebarVisible
+        })
+    }
+
+    testAlert() {
+        alert('The menu state has been changed!')
+    }
+
     render() {
         return (
             <div id="header">
             <div className="header-section">
-                <div id="menu-icon">
+                <div id="menu-icon" onClick={this.testAlert}>
                     <i className="material-icons clickable">menu</i>
                 </div>
                 <div id="title">Podcatch</div>
