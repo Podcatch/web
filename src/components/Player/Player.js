8f11d0b43e5f9f95203789b81ed51c8ea76d4147
Jelani Thompson <jelani.thompson@outlook.com>
2017-01-02 22:07:56 -0500

    Started working on playbar and added AudioJS

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,18 +1,32 @@
 import React from 'react'
+const load = require('audio-loader')
+const play = require('audio-play')
+const context = require('audio-context')
+let url = "http://feeds.gimletmedia.com/twiceremovedshow"
 
 class Player extends React.Component {
-    play(url) {
-        this.currentShow = new Audio(url)
-        this.currentShow.play()
+    constructor(props) {
+        super(props)
+        this.state = {
+            playing: false
+        }
     }
 
-    pause() {
-        this.currentShow.pause()
+    // Starting and stopping audio
+    play(url) {
+        load(url).then(play)
     }
 
     render() {
         return (
-            { /* Playbar and components */ }
+            <div id="player">
+                <div id="controls">
+                    <div id="play_pause"></div>
+                    <div id="time">
+                        <div id="playhead"></div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
