3d09677936001b53f4ef21819a1a5120fa08a85f
Jelani Thompson <jelani.thompson@outlook.com>
2017-01-03 23:10:46 -0500

    Begun working on Chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const podcasts = require('../../src/scripts/podcasts.js')
+
+class Chart extends React.Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
