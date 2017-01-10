40c7367700596edb523cd098ce4a2b54450fd004
Jelani Thompson <jelani.thompson@outlook.com>
2017-01-10 12:38:08 -0500

    Clarified purpose of charts

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 const podcasts = require('../../src/scripts/podcasts.js')
 
+// Charts will be for displaying top podcasts, trending podcasts, etc.
 class Chart extends React.Component {
     render() {
         return(
