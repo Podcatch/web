fbccc12537f253f324269ee24c3f33663cc7de4e
Jelani Thompson <jelani.thompson@outlook.com>
2017-01-21 21:42:38 -0500

    Assigned show IDs to keys

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+const feedParser = require('feedparser')
 let topFifty = 'https://itunes.apple.com/us/rss/toppodcasts/limit=50/json'
 
 // Charts will be for displaying top podcasts, trending podcasts, etc.
@@ -8,42 +9,49 @@ class Chart extends React.Component {
         this.state = {
             names: [],
             images: [],
-            summaries: []
+            summaries: [],
+            showID: []
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let that = this
         fetch(topFifty).then(function(response) {
             return response.json().then(function(json) {
                 let names = [],
                     images = [],
-                    summaries = []
+                    summaries = [],
+                    showID = []
                 json.feed.entry.forEach(function(datum) {
                     names.push(datum["im:name"].label)
                     images.push(datum["im:image"][0].label)
                     summaries.push(datum.summary.label)
+                    showID.push(datum.id.attributes["im:id"])
                 })
                 that.setState({
                     names: names,
                     images: images,
-                    summaries: summaries
+                    summaries: summaries,
+                    showID: showID
                 })
-                console.log(names)
-                console.log(images)
+                console.log(showID)
             })
         })
     }
 
     render() {
+        let showIds = this.state.showID
         return ( 
-            <div>
-                {this.state.images.map(function(showVal) {
-                    return <img src={showVal}></img>
-                })}
+            <div> 
+            {
+                this.state.images.map(function(showVal, i) {
+                    return <a href="http://podcatch.io/"><img src={showVal} key={showIds[i]}></img></a>
+                })
+            } 
             </div>
         )
     }
 }
 
+// TODO: Switch to fetching on server and then serving it to clients through a JSON API
 export default Chart
