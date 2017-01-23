6aa0bb73b92bffecefd6c4d177d1476741e9bdc0
Jelani Thompson <jelani.thompson@outlook.com>
2017-01-23 01:27:48 -0500

    Modified images

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -13,7 +13,7 @@ class Chart extends React.Component {
         let that = this
         fetch(topFifty).then(function(response) {
             return response.json().then(function(json) {
-                let names = [], images = [], modifiedImages = [], summaries = [], showID = [], featuredSize = '250x250'
+                let names = [], images = [], modifiedImages = [], summaries = [], showID = [], featuredSize = '200x200'
                 json.feed.entry.forEach(function(datum) {
                     names.push(datum["im:name"].label)
                     images.push(datum["im:image"][0].label)
